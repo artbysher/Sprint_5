ea6d8e691457524374f779a4f65bcbe01c630f49
Alexandra Sher <ya-sher-ka@yandex.ru>
2025-03-23 20:31:35 +0400

    URL для тестирования.

---
+++ curl.py
@@ -0,0 +1,3 @@
+main_site = 'https://stellarburgers.nomoreparties.site/'
+login_url = 'https://stellarburgers.nomoreparties.site/login'
+account_url= 'https://stellarburgers.nomoreparties.site/account/profile'
