d8fa521967afc878816d524ba0f126c77f42ff20
Alexandra Sher <ya-sher-ka@yandex.ru>
2025-03-24 00:23:52 +0400

    Изменила проверку вместо поиска элемента в самом меню, проверяем что tab стал активен.

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -11,15 +11,15 @@ class TestSectionNavigation:
         scroll_element = driver.find_element(*Locators.SCROLL_ELEMENT)
         driver.execute_script("arguments[0].scrollIntoView();", scroll_element)
         driver.find_element(*Locators.BUNS_BNT).click()
-        buns_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.BUNS_ELEMENT))
-        assert buns_element.is_displayed()
+        buns_active_tab = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.BUNS_ACTIVE_TAB))
+        assert buns_active_tab.is_displayed()
 
     def test_navigate_to_sauces_section(self, driver): # тест для перехода в раздел «Соусы»
         driver.find_element(*Locators.SAUCES_BNT).click()
-        sauces_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.SAUCES_ELEMENT))
-        assert sauces_element.is_displayed()
+        sauces_active_tab = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.SAUCES_ACTIVE_TAB))
+        assert sauces_active_tab.is_displayed()
 
     def test_navigate_to_toppings_section(self, driver): # тест для перехода в раздел «Начинки»
         driver.find_element(*Locators.TOPPINGS_BNT).click()
-        topping_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.TOPPINGS_ELEMENT))
-        assert topping_element.is_displayed()
+        topping_active_tab = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.TOPPINGS_ACTIVE_TAB))
+        assert topping_active_tab.is_displayed()
